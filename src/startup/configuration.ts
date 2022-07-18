dac594318bb36995d2f6ae00ef87256cb448d3da
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-07-18 13:34:55 +0200

    Fix firestore connection

--- src/startup/configuration.ts
+++ src/startup/configuration.ts
@@ -3,13 +3,13 @@ import winston from "winston";
 
 export default () => {
   try {
-    if (!config.get("private_key"))
+    if (!config.has("private_key"))
       winston.error("No private key provided for FrieStore connection");
-    if (!config.get("client_email"))
+    if (!config.has("client_email"))
       winston.error("No client email provided for FireStore connection");
-    if (!config.get("project_id"))
+    if (!config.has("project_id"))
       winston.error("No Provided project id for FireStore connection");
-    if (!config.get("FrontEndUrl"))
+    if (!config.has("FrontEndUrl"))
       winston.error("No front end url provided for integration");
   } catch (error) {
     winston.error({ startUpConfigurationError: error });
--- src/startup/db.ts
+++ src/startup/db.ts
@@ -5,26 +5,22 @@ import winston from "winston";
 
 export default () => {
   try {
-    // let serviceAccount: any = {
-    //   type: config.get("type"),
-    //   private_key: `${config.get("private_key")}`.replace(/\\n/g, "\n"),
-    //   client_email: config.get("client_email"),
-    //   project_id: config.get("project_id"),
-    //   private_key_id: config.get("private_key_id"),
-    //   client_id: config.get("client_id"),
-    //   auth_uri: config.get("auth_uri"),
-    //   token_uri: config.get("token_uri"),
-    //   auth_provider_x509_cert_url: config.get("auth_provider_x509_cert_url"),
-    //   client_x509_cert_url: config.get("client_x509_cert_url"),
-    // };
-    // console.log(serviceAccount);
-    initializeApp({
-      credential: cert(
-        require("../../unifood-356514-firebase-adminsdk-scup7-7953171ec5.json")
-      ),
-    });
-    const DB = getFirestore();
-    return DB;
+    if (
+      config.has("private_key") &&
+      config.has("client_email") &&
+      config.has("project_id")
+    ) {
+      let serviceAccount: any = {
+        private_key: `${config.get("private_key")}`.replace(/\\n/g, "\n"),
+        client_email: config.get("client_email"),
+        project_id: config.get("project_id"),
+      };
+      initializeApp({
+        credential: cert(serviceAccount),
+      });
+      const DB = getFirestore();
+      return DB;
+    }
   } catch (error) {
     winston.error({ startupDBError: error });
   }
